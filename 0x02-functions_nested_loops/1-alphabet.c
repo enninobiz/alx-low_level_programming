b3d2ff0a4a75109cf8750f0fc0281cf4591623c6
enninobiz <enninobiz@gmail.com>
2022-10-18 20:04:55 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet - print it in lowercase
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+}
