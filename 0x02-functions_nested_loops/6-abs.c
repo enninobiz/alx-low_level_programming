355db33e7cb7e3968a779d1659f4d41d58234e61
enninobiz <enninobiz@gmail.com>
2022-10-19 06:47:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#includ "main.h"
+
+/**
+ * _abs - computes the absolute value of an integar
+ * @t: The integer to be computed
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
