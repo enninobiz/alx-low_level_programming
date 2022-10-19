0c93ae6df3b522c27fb047255d9c4b4eabe98ce2
enninobiz <enninobiz@gmail.com>
2022-10-18 20:42:04 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet in lowercase
- *
- * Return: Always 0
+ * print_alphabet - print the alphabet in lowercase
  */
 void print_alphabet(void)
 {
@@ -13,6 +11,4 @@ void print_alphabet(void)
 		_putchar(lc);
 
 	_putchar('\n');
-
-	return (0);
 }
