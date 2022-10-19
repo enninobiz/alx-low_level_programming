0651647e01d9a1b81f3c21b8fdb8f61588de83b4
enninobiz <enninobiz@gmail.com>
2022-10-18 20:15:12 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
  * print alphabet - print it in lowercase
+ *Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -11,4 +12,6 @@ void print_alphabet(void)
 		_putchar(lc);
 
 	_putchar('\n');
+
+	return (0);
 }
